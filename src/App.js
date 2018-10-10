654d876f3b1752fb6fd02858d4b461671709c212
marcussantos <marcus.feliciano@gmail.com>
2018-10-10 00:28:03 -0300

    feat: initializing refectory, add BookCollection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import BookCollection from './components/BookCollection';
 
 class BooksApp extends React.Component {
   state = {
@@ -16,6 +17,7 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
+        <BookCollection collectionTitle={"Currently Reading"} />
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
